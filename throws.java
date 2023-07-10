e94060ef5bf2d3ecacbd0a2cbfe65604273200aa
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-10 10:06:30 +0530

    Create throws.java

---
+++ throws.java
@@ -0,0 +1,12 @@
+/* throws */
+class throwsDemo
+{
+    public static void main(String[] args) throws InterruptedException
+    {
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+    }
+}
